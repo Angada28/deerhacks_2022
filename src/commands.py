6c173e0bfd3e835143ca19d75bbe5832cf139612
matms <matm31415@gmail.com>
2022-04-30 16:12:49 -0400

    Relax eqn syntax.

--- src/commands.py
+++ src/commands.py
@@ -10,4 +10,4 @@ def dispatch_command(cmd: Command) -> Any:
     return COMMANDS_TO_IMPL[cmd.command](cmd.args)
 
 
-# print( dispatch_command(Parser().parse("$deermath solve y**2 - 3*y = 5 for y")))
+# print(dispatch_command(Parser().parse("$deermath solve y^2 - xy = 5 for y")))
--- src/parse_solve.py
+++ src/parse_solve.py
@@ -1,17 +1,27 @@
 from sympy import Eq, parse_expr
+from sympy.parsing.sympy_parser import standard_transformations, convert_xor, split_symbols, implicit_multiplication_application
 from dataclasses import dataclass
 from typing import Any
 
-@dataclass
+
+def parse_sympy_expr(e):
+    return parse_expr(
+        e,
+        transformations=([t for t in standard_transformations] + [convert_xor, implicit_multiplication_application]))
+
+
+@ dataclass
 class SolveArgs:
     var: str
     eqn: str
 
+
 def parse_equation(eqn: str) -> Eq:
     if '=' in eqn:
         lhs, rhs = eqn.split('=')
-        return Eq(parse_expr(lhs), parse_expr(rhs))
-    return Eq(parse_expr(eqn), 0)
+        return Eq(parse_sympy_expr(lhs), parse_sympy_expr(rhs))
+    return Eq(parse_sympy_expr(eqn), 0)
+
 
 def parse_solve(args: str) -> Any:
     fragments = list(map(lambda s: s.strip().casefold(), args.split()))
@@ -21,7 +31,7 @@ def parse_solve(args: str) -> Any:
         idx = fragments.index("for".casefold())
         eqn = ' '.join(fragments[:idx])
         var = fragments[idx + 1]
-    
+
         return SolveArgs(var, parse_equation(eqn))
 
     # solve eqn (assumes variable is x)
