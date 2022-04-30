b7c0b17f906879811062bc4e24bf0ed3ab624584
matms <matm31415@gmail.com>
2022-04-30 15:54:36 -0400

    Fix incorrect type hint.

--- src/parse_solve.py
+++ src/parse_solve.py
@@ -1,6 +1,6 @@
 from sympy import Eq, parse_expr
 from dataclasses import dataclass
-from typing import List
+from typing import Any
 
 @dataclass
 class SolveArgs:
@@ -13,7 +13,7 @@ def parse_equation(eqn: str) -> Eq:
         return Eq(parse_expr(lhs), parse_expr(rhs))
     return Eq(parse_expr(eqn), 0)
 
-def parse_solve(args: str) -> List[str]:
+def parse_solve(args: str) -> Any:
     fragments = list(map(lambda s: s.strip().casefold(), args.split()))
 
     # solve eqn for ...
