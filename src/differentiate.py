65ca9abe0fa61f750bdc2a490f4edb6da9881870
Angada28 <angad2011@hotmail.com>
2022-05-01 10:32:28 -0400

    paramaters fixed

--- src/differentiate.py
+++ src/differentiate.py
@@ -2,5 +2,5 @@ from parse_differentiate import DiffArgs
 import sympy
 
 
-def differentiate(args: DiffArgs):
+def differentiate(args: DiffArgs,temp):
     return sympy.diff(args.fn, sympy.symbols(args.var))
