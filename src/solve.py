bb09b1ddc53085017a13b73d8efc7c15dab42beb
matms <matm31415@gmail.com>
2022-05-01 11:16:31 -0400

    Fix where clauses in solve.

--- src/solve.py
+++ src/solve.py
@@ -6,9 +6,9 @@ def solve(args: SolveArgs, state):
     assert isinstance(args, SolveArgs)
     
     eq_tuples = [(e.lhs, e.rhs) for e in args.eqn]
-    for i, eq in enumerate(eq_tuples):
+    for i in range(len(eq_tuples)):
         for subst in args.where:
-            eqnl, eqnr = eq
+            eqnl, eqnr = eq_tuples[i]
             eqnl = eqnl.subs(subst[0], subst[1])
             eqnr = eqnr.subs(subst[0], subst[1])
             eq_tuples[i] = (eqnl, eqnr)
@@ -24,5 +24,5 @@ def solve(args: SolveArgs, state):
 if __name__ == "__main__":
     from parse import Parser
     from commands import dispatch_command
-    print(dispatch_command(Parser().parse("$deermath solve x + y + z = 20 where x=2y, y = 3z for z")))
-    print(dispatch_command(Parser().parse("$deermath solve x + y = 10, x - y = z, z=5 for x, y, z")))
+    print(dispatch_command(Parser().parse("$deermath solve x + y + z = 20 where x=2y, y = 3z for z"), ()))
+    print(dispatch_command(Parser().parse("$deermath solve x + y = 10, x - y = z, z=5 for x, y, z"), ()))
