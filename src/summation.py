256bfbc08be4e93d125e23a8ca70d40ac7246d1f
Zaina <45265333+zainah234@users.noreply.github.com>
2022-04-30 20:25:36 -0400

    Create summation.py

---
+++ src/summation.py
@@ -0,0 +1,13 @@
+from parse_summation import SumArgs
+from typing import Union
+from sympy.abc import i, k, m, n, x
+from sympy import Sum, factorial, oo, IndexedBase, Function, symbols, pretty_print
+
+
+def summation(args: Union[SumArgs, None]):
+    if args is None:
+        return 0
+    else:
+        return Sum(args.exp, (args.var, args.lower, args.upper)).doit()
+
+
